c6b9873ae437024585dbe890c022176c667735b9
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-07 17:24:22 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
+    sentence.end_with?(.)
     
   end
 
   def question?
+    question.end_with?(?)
 
   end
 
   def exclamation?
+    exclamation.end_with?(!)
 
   end
 
   def count_sentences
+    count_sentences.split
+    
 
   end
 end
